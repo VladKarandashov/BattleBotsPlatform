470c2e1b7cfe5d7703e916b49bed932b9bdc3975
karandashov <vlad.karandashov.tech@gmail.com>
2024-01-05 17:49:33 +0300

    start crm service

---
+++ crm-service/src/test/java/ru/abradox/crmservice/CrmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.abradox.crmservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
