da1343667629fee2762ab16d2e3e5a56211718d3
karandashov <vlad.karandashov.tech@gmail.com>
2024-01-08 10:47:25 +0300

    починил gateway

--- middleware-service/src/main/java/ru/abradox/middlewareservice/controller/UserController.java
+++ middleware-service/src/main/java/ru/abradox/middlewareservice/controller/UserController.java
@@ -3,6 +3,7 @@ package ru.abradox.middlewareservice.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.yaml.snakeyaml.util.UriEncoder;
@@ -13,6 +14,11 @@ public class UserController {
 
     @GetMapping(value = "/api/v1/whoami", produces = MediaType.APPLICATION_JSON_VALUE)
     public String whoami(@RequestHeader("user") String userInfo) {
+        return UriEncoder.decode(userInfo);
+    }
+
+    @PostMapping(value = "/api/v1/whoami", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String whoamiPOST(@RequestHeader("user") String userInfo) {
         return UriEncoder.decode(userInfo);
     }
 }
