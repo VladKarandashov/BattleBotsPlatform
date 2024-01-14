65f93d2ac3a7a27fe31b2754de4da055173f23bd
karandashov <vlad.karandashov.tech@gmail.com>
2024-01-14 11:13:47 +0300

    создал battle-gateway

---
+++ battle-gateway/src/main/java/ru/abradox/battlegateway/BattleGatewayApplication.java
@@ -0,0 +1,13 @@
+package ru.abradox.battlegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleGatewayApplication.class, args);
+	}
+
+}
