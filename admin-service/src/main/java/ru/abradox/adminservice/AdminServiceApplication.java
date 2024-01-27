e93f86b3c7827fc26d3c26e6872633fb02e91f66
karandashov <vlad.karandashov.tech@gmail.com>
2024-01-27 14:55:01 +0300

    прикрутил admin

--- admin-service/src/main/java/ru/abradox/adminservice/AdminServiceApplication.java
+++ admin-service/src/main/java/ru/abradox/adminservice/AdminServiceApplication.java
@@ -1,9 +1,11 @@
 package ru.abradox.adminservice;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableAdminServer
 @EnableEurekaServer
 @SpringBootApplication(scanBasePackages = "ru.abradox")
 public class AdminServiceApplication {
