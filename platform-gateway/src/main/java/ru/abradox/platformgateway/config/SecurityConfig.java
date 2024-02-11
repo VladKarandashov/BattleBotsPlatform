406af253ab3e0c5dc5dd5dff10f05c71944edc86
karandashov <vlad.karandashov.tech@gmail.com>
2024-02-11 18:55:22 +0300

    починили cors

--- platform-gateway/src/main/java/ru/abradox/platformgateway/config/SecurityConfig.java
+++ platform-gateway/src/main/java/ru/abradox/platformgateway/config/SecurityConfig.java
@@ -7,6 +7,12 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
@@ -17,7 +23,7 @@ class SecurityConfig {
     public SecurityWebFilterChain clientSecurityFilterChain(ServerHttpSecurity http) {
         return http
                 .oauth2Login(Customizer.withDefaults())
-                .cors(ServerHttpSecurity.CorsSpec::disable)
+                .cors(corsSpec -> corsSpec.configurationSource(corsConfigurationSource()))
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(authorize -> authorize
                         .pathMatchers("/").permitAll()
@@ -25,5 +31,21 @@ class SecurityConfig {
                         .pathMatchers("/actuator/**").permitAll()
                         .anyExchange().authenticated())
                 .build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource()
+    {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://147.45.107.29:8070", "http://147.45.107.29:8080"));
+        configuration.setMaxAge(168000L);
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        //or any domain that you want to restrict to
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "OPTIONS"));
+        //Add the method support as you like
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
