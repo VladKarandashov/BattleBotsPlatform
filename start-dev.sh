342361955d85b7afb1b70186d4111d363556f43d
karandashov <vlad.karandashov.tech@gmail.com>
2024-02-12 20:39:54 +0300

    сделал docker compose

--- start-dev.sh
+++ start-dev.sh
@@ -5,16 +5,4 @@ docker stop $(docker ps -aq)
 
 docker rm $(docker ps -aq)
 
-ENV_FILE="/home/abradox/work/BattleBotsConfig/app.env"
-
-docker run --network="host" --env-file "$ENV_FILE" -d admin-service-image
-sleep 5
-docker run --network="host" --env-file "$ENV_FILE" -d token-service-image
-docker run --network="host" --env-file "$ENV_FILE" -d battle-service-image
-docker run --network="host" --env-file "$ENV_FILE" -d statistic-service-image
-sleep 5
-docker run --network="host" --env-file "$ENV_FILE" -d middleware-service-image
-docker run --network="host" --env-file "$ENV_FILE" -d crm-service-image
-sleep 5
-docker run --network="host" --env-file "$ENV_FILE" -d platform-gateway-image
-docker run --network="host" --env-file "$ENV_FILE" -d battle-gateway-image
+docker-compose up -d
